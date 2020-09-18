0d56312f24488c47ebddbd317dca1ee3fc6c4140
Ben Edwards <benedwrds@yahoo.com>
2020-09-17 20:40:51 -0500

    reducer update

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,2 +1,12 @@
 import {combineReducers} from 'redux'
 
+const businesses = (state = [], action) => {
+  switch(action.type){
+    case 'ADD_BUSINESS': 
+      return [...state, action.value]
+    default: 
+      return state
+  }
+}
+
+export default combineReducers({ businesses })
